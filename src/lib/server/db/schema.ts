151b4f41f956d3e37b2d3958136f1e00d424922e
Lucas Doell <lucas@lux.dev>
2025-02-08 00:27:12 -0500

    Add better-auth integration

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,49 @@
-import { pgTable, serial, text, integer } from 'drizzle-orm/pg-core';
+import { boolean, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
 
 export const user = pgTable('user', {
-	id: serial('id').primaryKey(),
-	age: integer('age')
+	id: uuid().primaryKey(),
+	name: text().notNull(),
+	email: text().notNull(),
+	emailVerified: boolean().notNull(),
+	image: text().notNull(),
+	createdAt: timestamp().notNull().defaultNow(),
+	updatedAt: timestamp().notNull().defaultNow()
+});
+
+export const session = pgTable('session', {
+	id: uuid().primaryKey(),
+	userId: uuid().notNull(),
+	token: text().notNull(),
+	expiresAt: timestamp().notNull(),
+	ipAddress: text(),
+	userAgent: text(),
+	createdAt: timestamp().notNull().defaultNow(),
+	updatedAt: timestamp().notNull().defaultNow()
+});
+
+export const account = pgTable('account', {
+	id: uuid().primaryKey(),
+	userId: uuid()
+		.notNull()
+		.references(() => user.id),
+	accountId: text().notNull(),
+	providerId: text().notNull(),
+	accessToken: text(),
+	refreshToken: text(),
+	accessTokenExpiresAt: timestamp(),
+	refreshTokenExpiresAt: timestamp(),
+	scope: text(),
+	idToken: text(),
+	password: text(),
+	createdAt: timestamp().notNull().defaultNow(),
+	updatedAt: timestamp().notNull().defaultNow()
+});
+
+export const verification = pgTable('verification', {
+	id: uuid().primaryKey(),
+	identifier: text().notNull(),
+	value: text().notNull(),
+	expiresAt: timestamp().notNull(),
+	createdAt: timestamp().notNull().defaultNow(),
+	updatedAt: timestamp().notNull().defaultNow()
 });
