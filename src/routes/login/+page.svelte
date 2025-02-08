1fbdb20695e41a5742689d843ca2b3247dac03fd
Lucas Doell <lucas@lux.dev>
2025-02-08 01:14:11 -0500

    Add login route

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import LoginForm from '$lib/components/login-form.svelte';
+</script>
+
+<div class="flex w-full items-center justify-center px-4 py-16">
+	<LoginForm />
+</div>
